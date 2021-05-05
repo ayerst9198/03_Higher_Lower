83b845f29a3cf94539928f00e8a5d73e9724c9eb
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-05-05 13:20:01 +1200

    added easter eggs

--- 00_HL_Sandbox.py
+++ 00_HL_Sandbox.py
@@ -1,5 +1,172 @@
-import random
-loop = ""
-while loop == "":
-    thing = random.randint(1, 1000000000000000000)
-    print(thing + thing + thing)
+def lucky_you():
+
+    valid = False
+    while not valid:
+        import random
+
+
+        # functions go here
+        def statement_generator(statement, decoration):
+            sides = decoration * 3
+
+            statement = "{} {} {}".format(sides, statement, sides)
+            top_bottom = decoration * len(statement)
+
+            print(top_bottom)
+            print(statement)
+            print(top_bottom)
+
+            return ""
+
+
+        def yes_no(question):
+            valid = False
+            while not valid:
+                response = input(question).lower()
+
+                if response == "yes" or response == "y":
+                    response = "yes"
+                    return response
+
+                elif response == "no" or response == "n":
+                    response = "no"
+                    return response
+
+                else:
+                    print("I don't understand. "
+                        "Stop acting smart and just input Yes or No")
+
+
+        def cont():
+            input("Enter anything to continue the Game :D")
+
+
+        def instructions():
+            statement_generator("How to Play", "*")
+            print()
+            print("The aim of the game is to make as much money as possible!")
+            print()
+            print("It costs $1 to start a round.")
+            print()
+            print("A random token will be generated from a unicorn, horse, donkey, or zebra:")
+            print()
+            print("Unicorns are worth $4.")
+            print("If you get a Zebra or a Horse, you lose $0.50")
+            print("Watch out for Donkeys!")
+            print("If you get a Donkey, you lose $1!")
+            print()
+            print("When you run out of money; YOU LOSE")
+            print()
+            print("Enjoy!")
+            print()
+            cont()
+            return ""
+
+
+        def num_check(question, low, high):
+            error = "Please enter a whole number between 1 and 10\n"
+
+            valid = False
+            while not valid:
+                try:
+                    # ask the question
+                    response = int(input(question))
+                    # if amount is too low / too high give
+                    if low < response <= high:
+                        return response
+
+                    # output error
+                    else:
+                        print(error)
+                except ValueError:
+                    print(error)
+
+
+        # Main routine goes here...
+        statement_generator("Welcome to the Lucky Unicorn Game", "*")
+        print()
+
+        show_instructions = yes_no("Have you played this "
+                                "game before? ")
+
+        if show_instructions == "no":
+            instructions()
+
+        # Ask user how much they want to play with...
+        statement_generator("START", "+")
+        print()
+        how_much = num_check("How much would you "
+                            "like to play with between $1 and $10? ", 0, 10)
+        balance = how_much
+
+        rounds_played = 0
+        play_again = input("Please press <Enter> to play... ").lower()
+        while play_again == "":
+
+            # increase # of rounds played
+            rounds_played += 1
+
+            # print round number
+            print()
+            statement_generator("Round #{}".format(rounds_played), "*")
+            chosen_num = random.randint(1, 100)
+            print()
+
+            # Adjust balance
+
+            # if the random # is between 1 and 5,
+            # user gets a unicorn (add $4 to the balance)
+            if 1 <= chosen_num <= 5:
+                chosen = "unicorn"
+                balance += 4
+
+            # if the random # is between 6 and 36,
+            # user gets a donkey (subtract $1 from balance)
+            elif 6 <= chosen_num <= 36:
+                chosen = "donkey"
+                balance -= 1
+
+            # token is either horse or zebra...
+            # either way subtract $0.5 from balance
+            else:
+                # if the number is even, set the chosen
+                # item to horse
+                if chosen_num % 2 == 0:
+                    chosen = "horse"
+                # if the number is odd, set the chosen
+                # item to zebra
+                else:
+                    chosen = "zebra"
+                balance -= 0.5
+
+            if chosen == "unicorn":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "$")
+                print()
+            elif chosen == "horse":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "-")
+                print()
+            elif chosen == "zebra":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "%")
+                print()
+            elif chosen == "donkey":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "^")
+                print()
+            else:
+                play_again = "xxx"
+                print("You have somehow broken my game.")
+                print("This is an error message and the game will now shut down.")
+
+            if balance < 1:
+                play_again = "xxx"
+                print("You have run out of money.")
+                print(statement_generator("Game Over", "'"))
+            else:
+                play_again = input("Press <Enter> to play again "
+                                "or 'xxx' to quit ")
+
+        print()
+        statement_generator("Final Results", "=")
+        statement_generator("Final Balance: ${}".format(balance), ".")
+        print("Thank you for playing")
+
+lucky_you()
--- 01_HL_Base_Component_v1.py
+++ 01_HL_Base_Component_v1.py
@@ -3,14 +3,14 @@ import math
 # functions go here
 
 # Number checking function makes sure integer above 0 is entered
-def num_check(question, error, low=None, high=None):
+def num_check(question, error, low=None, high=None, exit_code=None, konami_code=None, impossible_mode=None):
 
     valid = False
     while not valid:
         try:
             response = input(question).lower()
 
-            if response == "xxx":
+            if response == exit_code:
                 return response
             else:
                 response = int(response)
@@ -47,32 +47,6 @@ def num_check(question, error, low=None, high=None):
             print(error)
             print()
 
-# checks how many rounds the user wants to play
-def check_rounds():
-    while True:
-        response = input("How many rounds: ")
-
-        round_error = "Please type either <enter> " \
-                      "or an integer that is more than 0"
-        if response != "":
-            try:
-                if response == "UpUpDownDownLeftRightLeftRightBAStart":
-                    return response
-                elif response == "i":
-                    return response
-                else:
-                    response = int(response)
-                
-                if response < 1:
-                    print(round_error)
-                    continue
-
-            except ValueError:
-                print(round_error)
-                continue
-
-        return response
-
 
 # puts a decorative border around statements
 def statement_generator(statement, side_decoration, top_bottom_decoration):
@@ -103,6 +77,10 @@ def yes_no(question):
         elif response == "no" or response == "n":
             response = "no"
             return response
+        
+        elif response == "rps":
+            response = "RPS"
+            return response
 
         else:
             print("Please input yes / no")
@@ -113,6 +91,476 @@ def instructions():
     statement_generator("How to play", "*", "?")
     print("Instructions")
     return ""
+
+
+# Easter egg game inside easter egg game
+def lucky_you():
+
+    valid = False
+    while not valid:
+        import random
+
+
+        # functions go here
+        def statement_generator(statement, decoration):
+            sides = decoration * 3
+
+            statement = "{} {} {}".format(sides, statement, sides)
+            top_bottom = decoration * len(statement)
+
+            print(top_bottom)
+            print(statement)
+            print(top_bottom)
+
+            return ""
+
+
+        def yes_no(question):
+            valid = False
+            while not valid:
+                response = input(question).lower()
+
+                if response == "yes" or response == "y":
+                    response = "yes"
+                    return response
+
+                elif response == "no" or response == "n":
+                    response = "no"
+                    return response
+
+                else:
+                    print("I don't understand. "
+                        "Stop acting smart and just input Yes or No")
+
+
+        def cont():
+            input("Enter anything to continue the Game :D")
+
+
+        def instructions():
+            statement_generator("How to Play", "*")
+            print()
+            print("The aim of the game is to make as much money as possible!")
+            print()
+            print("It costs $1 to start a round.")
+            print()
+            print("A random token will be generated from a unicorn, horse, donkey, or zebra:")
+            print()
+            print("Unicorns are worth $4.")
+            print("If you get a Zebra or a Horse, you lose $0.50")
+            print("Watch out for Donkeys!")
+            print("If you get a Donkey, you lose $1!")
+            print()
+            print("When you run out of money; YOU LOSE")
+            print()
+            print("Enjoy!")
+            print()
+            cont()
+            return ""
+
+
+        def num_check(question, low, high):
+            error = "Please enter a whole number between 1 and 10\n"
+
+            valid = False
+            while not valid:
+                try:
+                    # ask the question
+                    response = int(input(question))
+                    # if amount is too low / too high give
+                    if low < response <= high:
+                        return response
+
+                    # output error
+                    else:
+                        print(error)
+                except ValueError:
+                    print(error)
+
+
+        # Main routine goes here...
+        statement_generator("Welcome to the Lucky Unicorn Game", "*")
+        print()
+
+        show_instructions = yes_no("Have you played this "
+                                "game before? ")
+
+        if show_instructions == "no":
+            instructions()
+
+        # Ask user how much they want to play with...
+        statement_generator("START", "+")
+        print()
+        how_much = num_check("How much would you "
+                            "like to play with between $1 and $10? ", 0, 10)
+        balance = how_much
+
+        rounds_played = 0
+        play_again = input("Please press <Enter> to play... ").lower()
+        while play_again == "":
+
+            # increase # of rounds played
+            rounds_played += 1
+
+            # print round number
+            print()
+            statement_generator("Round #{}".format(rounds_played), "*")
+            chosen_num = random.randint(1, 100)
+            print()
+
+            # Adjust balance
+
+            # if the random # is between 1 and 5,
+            # user gets a unicorn (add $4 to the balance)
+            if 1 <= chosen_num <= 5:
+                chosen = "unicorn"
+                balance += 4
+
+            # if the random # is between 6 and 36,
+            # user gets a donkey (subtract $1 from balance)
+            elif 6 <= chosen_num <= 36:
+                chosen = "donkey"
+                balance -= 1
+
+            # token is either horse or zebra...
+            # either way subtract $0.5 from balance
+            else:
+                # if the number is even, set the chosen
+                # item to horse
+                if chosen_num % 2 == 0:
+                    chosen = "horse"
+                # if the number is odd, set the chosen
+                # item to zebra
+                else:
+                    chosen = "zebra"
+                balance -= 0.5
+
+            if chosen == "unicorn":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "$")
+                print()
+            elif chosen == "horse":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "-")
+                print()
+            elif chosen == "zebra":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "%")
+                print()
+            elif chosen == "donkey":
+                statement_generator("You got a {}. Your balance is ${:.2f}".format(chosen, balance), "^")
+                print()
+            else:
+                play_again = "xxx"
+                print("You have somehow broken my game.")
+                print("This is an error message and the game will now shut down.")
+
+            if balance < 1:
+                play_again = "xxx"
+                print("You have run out of money.")
+                print(statement_generator("Game Over", "'"))
+            else:
+                play_again = input("Press <Enter> to play again "
+                                "or 'xxx' to quit ")
+
+        print()
+        statement_generator("Final Results", "=")
+        statement_generator("Final Balance: ${}".format(balance), ".")
+        print("Thank you for playing")
+
+
+# Easter Egg Game
+def play_game():
+
+    valid = False
+    while not valid:
+        import random
+        # functions go here
+
+
+        # makes statements look good P.S. I MADE THIS CODE
+        def statement_generator(statement, left_side_decoration, top_decoration, bottom_decoration, right_side_decoration):
+
+            left_sides = left_side_decoration * 3
+
+            right_sides = right_side_decoration * 3
+
+            statement = "{} {} {}".format(left_sides, statement, right_sides)
+
+            top_bottom = top_decoration * len(statement)
+
+            bottom_bottom = bottom_decoration * len(statement)
+
+            print(top_bottom)
+            print(statement)
+            print(bottom_bottom)
+
+            return ""
+        
+        
+
+
+        #  simplifies instructions
+        def instructions():
+            statement_generator("How to play", "*", "?", "?", "*")
+            print()
+            print("Choose either a number of rounds or press <enter> for")
+            print("  infinite mode, or i for infinite impossible mode")
+            print()
+            print("Then for each round, choose from rock")
+            print("/ paper / scissors (or xxx to quit)")
+            print("You can type r / p / s / x if you")
+            print("don't want to type the entire word")
+            print()
+            print("The rules are...")
+            print("- Rock beats scissors")
+            print("- Scissors beats paper")
+            print("- Paper beats rock")
+            print()
+            statement_generator("Have fun", "^", "*", "-", "*")
+            return ""
+
+
+        # confines answers to yes / no
+        def yes_no(question):
+            valid = False
+            while not valid:
+                response = input(question).lower()
+
+                if response == "yes" or response == "y":
+                    response = "yes"
+                    return response
+
+                elif response == "no" or response == "n":
+                    response = "no"
+                    return response
+                
+                elif response == "lucky you":
+                    response = "LU"
+                    return response
+
+                else:
+                    print("Please input yes / no")
+
+
+        # checks rock, paper, or scissors
+        def choice_checker(question, valid_list, error):
+
+            valid = False
+            while not valid:
+
+                # Ask user for choice (and put choice in lowercase)
+                response = input(question).lower()
+
+                # iterates through list and if response is an item
+                # in the list (or the first letter of an iten), the
+                # full item name is returned
+
+                for item in valid_list:
+                    if response == item[0] or response == item:
+                        return item
+
+                # output error if item not in list
+                print(error)
+                print()
+
+
+        # checks rounds or infinite
+        def check_rounds():
+            while True:
+                response = input("How many rounds: ")
+
+                round_error = "Please type either <enter> " \
+                            "or an integer that is more than 0"
+                if response != "":
+                    try:
+                        if response == "i":
+                            return response
+                        else:
+                            response = int(response)
+
+                        if response < 1:
+                            print(round_error)
+                            continue
+
+                    except ValueError:
+                        print(round_error)
+                        continue
+
+                return response
+
+        # Main routine goes here
+
+
+        rounds_lost = 0
+        rounds_drawn = 0
+        rounds_played = 0
+        result = "quitter"
+
+        # Lists of valid responses
+
+        yes_no_list = ["yes", "no"]
+        rps_list = ["rock", "paper", "scissors", "xxx"]
+        game_summary = []
+
+        # welcome user
+        statement_generator("Welcome to Rock Paper Scissors", "|", "^", "-", "|")
+        print()
+
+        # Asks user if they have played before.
+        show_instructions = yes_no("Have you played my "
+                                "game before? ")
+
+        # If 'no' show instructions
+        if show_instructions == "no":
+            print()
+            instructions()
+
+
+        elif show_instructions == "LU":
+            print()
+            lucky_you()
+            
+        # Ask user for # of rounds, <enter> for infinite mode
+        rounds = check_rounds()
+
+
+        # ask user for # of rounds then loop
+        end_game = "no"
+        while end_game == "no":
+
+            # Rounds heading
+
+            # Check if Continuous or impossible
+
+            print()
+            if rounds == "":
+                heading = statement_generator("Continuous Mode: Round {}".format(rounds_played + 1), "!", "!", "!", "!")
+
+            elif rounds == "i":
+                heading = statement_generator("Impossible Mode: Round {}".format(rounds_played + 1), "💀", "", "", "💀")
+
+
+            else:
+                heading = "Round {} of {}".format(rounds_played + 1, rounds)
+                # End game if exit code is typed
+                if rounds_played == "":
+                    end_game = "yes"
+                    statement_generator(heading, "*", "*", "*", "*")
+
+
+            choose_instruction = "Please choose rock (r), paper " \
+                                "(p), or scissors (s): "
+
+            choose_error = "Please choose from rock / " \
+                            "paper / scissors (or xxx to quit)"
+
+            # Ask user for choice and check it's valid
+
+            choose = choice_checker(choose_instruction, rps_list,
+                                    choose_error)
+
+            if choose == "xxx":
+                break
+
+            # get computer choice
+            comp_choice = random.choice(rps_list[:-1])
+
+            # compare choices
+            if rounds == "i":
+                if choose == "rock":
+                    comp_choice = "paper"
+                    result = "lose"
+                elif choose == "paper":
+                    comp_choice = "scissors"
+                    result = "lose"
+
+                else:
+                    comp_choice = "paper"
+                    result = "lose"
+
+            else:
+                if choose == comp_choice:
+                    result = "tied"
+
+                elif choose == "rock" and comp_choice == "paper":
+                    result = "lose"
+
+                elif choose == "paper" and comp_choice == "scissors":
+                    result = "lose"
+
+                elif choose == "scissors" and comp_choice == "rock":
+                    result = "lose"
+
+                else:
+                    result = "win"
+
+                if result == "lose":
+                    rounds_lost += 1
+                elif result == "tied":
+                    rounds_drawn += 1
+
+            # add result to list
+            game_summary.append(result)
+
+            #  End game if exit code is typed
+
+            if result == "tied":
+                feedback = " It's a tie "
+            elif result == "lose":
+                feedback = "You chose {} and com chose {} -  you {} ".format(choose, comp_choice, result)
+            else:
+                feedback = "You chose {} and com chose {} -  you {} ".format(choose, comp_choice, result)
+
+
+            # rest of loop / game
+
+            print(feedback)
+            print()
+
+            rounds_played += 1
+
+            # end game if requested # of rounds has been played
+            if rounds_played == rounds:
+                break
+
+        # Display Game Summary
+
+        rounds_won = rounds_played - rounds_lost - rounds_drawn
+        print("***** Summary *****")
+        print("Won: {} \t|\t Lost: {} \t|\t Draw: "
+            "{}".format(rounds_won, rounds_lost, rounds_drawn))
+        print()
+        print("Thank you for playing")
+
+        # Ask user if they want to see their game history
+        history = yes_no("Do you want to see your history? ")
+        # If 'yes' show history
+        if history == "yes":
+            if result == "quitter":
+                print()
+                statement_generator("lol, you are a quitting loser", "😭", "", "", "😭")
+
+            else:
+                rounds_won = rounds_played - rounds_drawn - rounds_lost
+
+                # calculate game stats
+                percent_win = rounds_won / rounds_played * 100
+                percent_lose = rounds_lost / rounds_played * 100
+                percent_tie = rounds_drawn / rounds_played * 100
+
+                print()
+                statement_generator("***** Game History *****", "|", "*", "-", "|")
+                for game in game_summary:
+                    print(game)
+
+                print()
+
+                # displays game stats with % values to the nearest whole number
+                statement_generator("Game Statistics", "*", "?", "-", "*")
+                print("Win: {}, ({:.0f}%)\nLoss: {}, "
+                    "({:.0f}%)\nTie: {}, ({:.0f}%)".format(rounds_won, percent_win, rounds_lost,
+                                                            percent_lose, rounds_drawn, percent_tie))
+                print("Thank you for playing")
+        else:
+            print("Thank you for playing")
 
 # Main routine
 
@@ -127,11 +575,23 @@ if show_instructions == "yes":
 elif show_instructions == "no":
     instructions()
 
+elif show_instructions == "RPS":
+    RPS = "true"
+    while RPS == "true":
+        ask_game = yes_no("Do you want to play a game? " )
+        if ask_game == "yes":
+            play_game()
+        elif ask_game == "no":
+            RPS = "False"
+            break
+        
+
+
 # asks for number of rounds
 rounds_lost = 0
 rounds_drawn = 0
 rounds_played = 0
-rounds = check_rounds()
+rounds = num_check("How many rounds? ", "oops", 0, exit_code="")
 # list to hold user guesses and help prevent duplicates
 already_guessed = []
 
@@ -141,8 +601,8 @@ lowest = num_check("Low Number: ", "Enter an integer above 0", 1)
 highest = num_check("High Number: ", "Please enter a integer above {}".format(lowest), lowest + 1)
 
 # calculates maximum number of guesses based on binary search strategy
-range = highest - lowest + 1
-max_raw = math.log2(range)
+num_range = highest - lowest + 1
+max_raw = math.log2(num_range)
 max_upped = math.ceil(max_raw)
 max_guesses = max_upped + 1
 
@@ -157,7 +617,7 @@ play_again = "yes"
 
 # makes sure that user plays for specified amount of rounds
 while play_again == "yes":
-    while rounds != rounds_played - 1:
+    # while rounds != rounds_played - 1:
         
         # displays round banner
         print()
@@ -176,7 +636,10 @@ while play_again == "yes":
                 print("Max Guesses: {}".format(max_guesses))
                 already_guessed = []
 
-                guess = num_check("Guess: ".format(guesses_allowed), "Please enter an integer between {} and {}".format(lowest, highest))
+                guess_instruction ="Guess: "
+                guess_error = "Please enter a number between {} and {}".format(lowest, highest)
+
+                guess = num_check(guess_instruction, guess_error, lowest, highest, "xxx")
 
                 # checks that guess is not a duplicate
                 if guess in already_guessed:
@@ -242,7 +705,10 @@ while play_again == "yes":
                 guesses_allowed = "infinity"
 
                 print("You got it right")
-                break
+                if guess == "xxx":
+                    play_again = "no"
+                    print("you quit lol")
+                    break
             
             # impossible mode
 
