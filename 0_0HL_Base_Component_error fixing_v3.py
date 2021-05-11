5e8ca147755789cdcf28f8afbf6fd864b0a06610
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-05-11 14:50:07 +1200

    halp

--- 0_0HL_Base_Component_error fixing_v3.py
+++ 0_0HL_Base_Component_error fixing_v3.py
@@ -2,13 +2,9 @@ import random
 import math
 # functions go here
 
+
 # Number checking function makes sure integer above 0 is entered
 def num_check(question, error, low=None, high=None, exit_code=None):
-
-
-    print("this is a number checker")
-
-    print("Low: ", low * 4)
 
     valid = False
     while not valid:
@@ -22,7 +18,6 @@ def num_check(question, error, low=None, high=None, exit_code=None):
             else:
                 response = int(response)
  
-
             if low is not None and high is not None:
                 if low <= response <= high:
                     return response
@@ -33,7 +28,7 @@ def num_check(question, error, low=None, high=None, exit_code=None):
                     continue
 
             elif low is not None:
-                if response >= 0:
+                if response >= low:
                     return response
                 else:
                     print(error)
@@ -47,7 +42,6 @@ def num_check(question, error, low=None, high=None, exit_code=None):
                     print(error)
                     print()
                     continue
-
 
             else:
                 return response
@@ -73,6 +67,7 @@ def statement_generator(statement, side_decoration, top_bottom_decoration):
 
     return ""
 
+
 # confines answers to yes / no
 def yes_no(question):
     valid = False
@@ -89,6 +84,7 @@ def yes_no(question):
 
         else:
             print("Please input yes / no")
+
 
 # confines answer to either 1 or 2 for mode choice
 def mode_checker(question, error):
@@ -111,6 +107,7 @@ def mode_checker(question, error):
             print(error)
             print()
 
+
 # Instructions
 def instructions():
     statement_generator("How to play", "*", "?")
@@ -121,18 +118,18 @@ def instructions():
     print("You can choose the lowest boundary of guesses and highest boundary of guesses")
     return ""
 
-
-
 # Main routine
+
 
 # ask user if they have played before
 show_instructions = yes_no("Have you played before? ")
 print()
 
-# List for game summaey from RPS 
+# List for game summaey from RPS
 game_summary = []
 # if user inputs no, show instructions
 if show_instructions == "no":
+
     instructions()
     
       
@@ -187,10 +184,134 @@ while play_again == "yes":
         rounds = 5
 
     while rounds_played < rounds:
-        print("Round {}".format(rounds_played + 1))
-        play_again = input("Again? yes / no")
 
         if mode == 2:
             rounds += 1
 
-        rounds_played += 1
+        print()
+
+        print("*** Round {} *****".format(rounds_played + 1))
+        print("Spoiler Alert", secret)  # remove later
+
+
+        # user inputs a guess, w/ error
+        print()
+        guess = num_check("Guess: ", "Please enter an integer between {} and {}".format(lowest, highest), exit_code="xxx")
+        print()
+        
+        if guess < lowest:
+            print("Please enter a number between {} and {}".format(lowest, highest))
+        elif guess > highest:
+            print("Please enter a number between {} and {}".format(lowest, highest))
+        # checks that guess is not a duplicate
+        if guess in already_guessed:
+            print("You have already guessed that number! Please try again.")
+            print("You *still* have {} guesses left".format(guesses_allowed))
+
+
+        if rounds == rounds_played:
+            play_again = "no"
+            print("GAME OVER")
+            break
+
+
+        # adds guess to list to prevent duplicate guesses
+        already_guessed.append(guess)
+
+        # Checks answer and compares it to hidden number
+        if guesses_allowed >= 1 and guess != secret:                    
+
+            # quitting mechanism if user inputs xxx
+            if guess == "xxx":
+                play_again = "no"
+                print("You Quit")
+                break
+
+            # if the guess is less than the answer, tell them to try a higher num
+            if guess < secret:
+                print("**Too low, try a higher number**")
+                print()
+                guesses_allowed -= 1
+
+            # if the guess is higher than the answer, tell them to try a lower num
+            elif guess > secret:
+                print("**Too high, try a lower number**")
+                print()
+                guesses_allowed -= 1
+
+            # displays how many guesses user has left
+            print("Guesses Left: {}".format(guesses_allowed))
+
+
+        # if user runs out of guesses and final guess is lower than ans:
+        if guess < secret:
+            print("**Too low! Game Over!**")
+            print("Answer; {}".format(secret))
+            result = "lose"
+            rounds_played += 1
+            game_summary.append(result)
+            rounds_lost += 1
+            already_guessed.clear()
+            break
+        
+        # if user runs out of guesses and final guess is higher than ans:
+        elif guess > secret:
+            print("**Too high! Game Over!**")
+            print("Answer; {}".format(secret))
+            rounds_played += 1
+            result = "lose"
+            game_summary.append(result)
+            already_guessed.clear()
+            rounds_lost += 1
+            break
+
+    
+        # if user guesses ans, congatulatulate them and end game
+        if guess == secret:
+            print("You got it right")
+            rounds_won += 1
+            print()
+            result = "win"
+            game_summary.append(result)
+            already_guessed.clear()
+            rounds_played += 1
+        
+
+
+# **** Calculate Game Stats ****
+percent_win = rounds_won / rounds_played * 100
+percent_lose = rounds_lost / rounds_played * 100
+
+# Asks user if they want to see there history
+show_history = yes_no("Would you like to see game history? ")
+
+# displays history if user says yes
+if show_history == "yes":
+    print()
+    print("**** Game History ****")
+    for game in game_summary:
+        print(game)
+
+
+
+# Doesnt display history if user says no
+elif show_history == "no":
+    print()
+    print("Thanks for Playing")
+
+# asks user if they want to see the game stats
+game_results = yes_no("Do you want to see your game stats? ")
+
+if game_results == "yes":
+
+    # Displays game stats with % values to the nearest whole number
+    print()
+    print("**** Game Statistics ****")
+    print("Win: {}: ({:.0f}%)\nLoss: {}: ({:.0f}%)".format(rounds_won, percent_win, rounds_lost, percent_lose))
+    print()
+    print()
+    print("Thanks for playing")
+    
+elif game_results == "no":
+    print()
+    print("Thanks for playing")
