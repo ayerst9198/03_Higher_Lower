95c0267a9f95f419b6928ad00b376a5a84db8fa6
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-05-17 09:56:14 +1200

    Game complete
    deco started

--- 0_0HL_Base_Component_error fixing_v3.py
+++ 0_0HL_Base_Component_error fixing_v3.py
@@ -110,13 +110,22 @@ def mode_checker(question, error):
 
 # Instructions
 def instructions():
-    statement_generator("How to play", "*", "?")
-    print("Welcome to Higher Lower.")
+    statement_generator("How to PLay", "*", "-")
     print()
-    print("The goal of this game is to find the correct number")
-    print()
-    print("You can choose the lowest boundary of guesses and highest boundary of guesses")
-    return ""
+    print(
+        "In this game your goal is too guess the\n"
+        "randomly generated number between a high and low or your choice\n"
+        "\n"
+        "There are 2 modes:\n"
+        "\n"
+        "Mode 1: Normal Mode\n"
+        "Mode 2: Infinite Mode\n"
+        "\n"
+        "To quit, you can input xxx to exit game\n"
+        "at anytime while guessing."
+    )
+    return""
+
 
 # Main routine
 
@@ -129,11 +138,14 @@ print()
 game_summary = []
 # if user inputs no, show instructions
 if show_instructions == "no":
-
     instructions()
+    print()
     
       
-mode = mode_checker("Do you want to play mode 1 or mode 2? ", "PLease enter either '1' or '2'")
+mode = mode_checker(
+    "Do you want to play mode 1 (normal mode), \n"
+    "or mode 2 (infinite mode)? ", "PLease enter either '1' or '2'"
+)
 
 
 # Initial setup gets range
@@ -149,19 +161,26 @@ rounds_lost = 0
 rounds_played = 0
 rounds_won = 0
 
+# Gets number of rounds
+if mode == 1:
+    # ask how many rounds user wants to play
+    rounds = num_check("How many rounds? ", "oops", 0, exit_code="")
+    print()
+elif mode == 2:
+    rounds = 5
 
 # list to hold user guesses and help prevent duplicates
 already_guessed = []
-
 
 # calculates maximum number of guesses based on binary search strategy
 num_range = highest - lowest + 1
 max_raw = math.log2(num_range)
 max_upped = math.ceil(max_raw)
 max_guesses = max_upped + 1
+guesses_allowed = max_guesses
 
 # display how many guesses the user has
-print("Max Guesses per round: {}".format(max_guesses))
+statement_generator("Max Guesses per round: {}".format(max_guesses), "*", "")
 print()
 
 # loops the code
@@ -171,110 +190,107 @@ play_again = "yes"
 # makes sure that user plays for specified amount of rounds
 while play_again == "yes":
     
-    # randomizes the answer from the high and low numbers given
-    ans = random.randint(lowest, highest)
-    # gets answer in single variable
-    secret = ans
-
-    if mode == 1:
-        # ask how many rounds user wants to play
-        rounds = num_check("How many rounds? ", "oops", 0, exit_code="")
-        print()
-    elif mode == 2:
-        rounds = 5
-
     while rounds_played < rounds:
 
+        # randomizes the answer from the high and low numbers given
+        secret = random.randint(lowest, highest) 
+        
         if mode == 2:
             rounds += 1
 
         print()
 
-        print("*** Round {} *****".format(rounds_played + 1))
-        print("Spoiler Alert", secret)  # remove later
+        statement_generator(" Round {} ".format(rounds_played + 1), "*", "")
 
-
-        # user inputs a guess, w/ error
-        print()
-        guess = num_check("Guess: ", "Please enter an integer between {} and {}".format(lowest, highest), exit_code="xxx")
-        print()
-        
-        if guess < lowest:
-            print("Please enter a number between {} and {}".format(lowest, highest))
-        elif guess > highest:
-            print("Please enter a number between {} and {}".format(lowest, highest))
-        # checks that guess is not a duplicate
-        if guess in already_guessed:
-            print("You have already guessed that number! Please try again.")
-            print("You *still* have {} guesses left".format(guesses_allowed))
-
-
-        if rounds == rounds_played:
-            play_again = "no"
-            print("GAME OVER")
-            break
-
-
-        # adds guess to list to prevent duplicate guesses
-        already_guessed.append(guess)
-
-        # Checks answer and compares it to hidden number
-        if guesses_allowed >= 1 and guess != secret:                    
+        #  ask user to guess and loop until they are correct / lose
+        while guess != secret: 
+            # user inputs a guess, w/ error
+            print()
+            guess = num_check("Guess: ", "Please enter an integer between {} and {}".format(lowest, highest), exit_code="xxx")
+            print()
 
             # quitting mechanism if user inputs xxx
             if guess == "xxx":
                 play_again = "no"
-                print("You Quit")
+                rounds_played = rounds + 1
+                statement_generator("You Quit", "-", "")
+                print()
+                break
+            
+            if guess != secret:
+                if guess < lowest:
+                    print("Please enter a number between {} and {}".format(lowest, highest))
+                if guess > highest:
+                    print("Please enter a number between {} and {}".format(lowest, highest))
+                # checks that guess is not a duplicate
+                if guess in already_guessed:
+                    print("You have already guessed that number! Please try again.")
+                    print("You *still* have {} guesses left".format(guesses_allowed))
+                
+
+
+            if rounds == rounds_played:
+                play_again = "no"
+                statement_generator("GAME OVER", "-", "")
                 break
 
-            # if the guess is less than the answer, tell them to try a higher num
-            if guess < secret:
-                print("**Too low, try a higher number**")
-                print()
-                guesses_allowed -= 1
 
-            # if the guess is higher than the answer, tell them to try a lower num
-            elif guess > secret:
-                print("**Too high, try a lower number**")
-                print()
-                guesses_allowed -= 1
+            # adds guess to list to prevent duplicate guesses
+            already_guessed.append(guess)
 
-            # displays how many guesses user has left
-            print("Guesses Left: {}".format(guesses_allowed))
+            # Checks answer and compares it to hidden number
+            if guesses_allowed >= 1 and guess != secret:                    
 
+                
 
-        # if user runs out of guesses and final guess is lower than ans:
-        if guess < secret:
-            print("**Too low! Game Over!**")
-            print("Answer; {}".format(secret))
-            result = "lose"
-            rounds_played += 1
-            game_summary.append(result)
-            rounds_lost += 1
-            already_guessed.clear()
-            break
-        
-        # if user runs out of guesses and final guess is higher than ans:
-        elif guess > secret:
-            print("**Too high! Game Over!**")
-            print("Answer; {}".format(secret))
-            rounds_played += 1
-            result = "lose"
-            game_summary.append(result)
-            already_guessed.clear()
-            rounds_lost += 1
-            break
+                # if the guess is less than the answer, tell them to try a higher num
+                if guess < secret:
+                    statement_generator("Too low, try a higher number", "*", "")
+                    print()
+                    guesses_allowed -= 1
 
-    
-        # if user guesses ans, congatulatulate them and end game
-        if guess == secret:
-            print("You got it right")
-            rounds_won += 1
-            print()
-            result = "win"
-            game_summary.append(result)
-            already_guessed.clear()
-            rounds_played += 1
+                # if the guess is higher than the answer, tell them to try a lower num
+                elif guess > secret:
+                    statement_generator("Too high, try a lower number", "*", "")
+                    print()
+                    guesses_allowed -= 1
+
+                # displays how many guesses user has left
+                statement_generator("Guesses Left: {}".format(guesses_allowed), "g", "-")
+
+            else:
+                # if user runs out of guesses and final guess is lower than ans:
+                if guess < secret:
+                    statement_generator("Too low! Game Over!", "*", "")
+                    print("Answer; {}".format(secret))
+                    result = "lose"
+                    rounds_played += 1
+                    game_summary.append(result)
+                    rounds_lost += 1
+                    already_guessed.clear()
+                    break
+                
+                # if user runs out of guesses and final guess is higher than ans:
+                elif guess > secret:
+                    statement_generator("Too high! Game Over!", "*", "")
+                    print("Answer; {}".format(secret))
+                    rounds_played += 1
+                    result = "lose"
+                    game_summary.append(result)
+                    already_guessed.clear()
+                    rounds_lost += 1
+                    break
+
+            
+                # if user guesses ans, congatulatulate them and end game
+                if guess == secret:
+                    statement_generator("You got it right", "!", "-")
+                    rounds_won += 1
+                    print()
+                    result = "win"
+                    game_summary.append(result)
+                    already_guessed.clear()
+                    rounds_played += 1
         
 
 
@@ -288,7 +304,7 @@ show_history = yes_no("Would you like to see game history? ")
 # displays history if user says yes
 if show_history == "yes":
     print()
-    print("**** Game History ****")
+    statement_generator("Game History", "*", "*")
     for game in game_summary:
         print(game)
 
